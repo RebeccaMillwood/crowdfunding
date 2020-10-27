1bbee35e4449a54532e9f7606bd157cc7c237530
RebeccaMillwood <bec-millwood@live.com.au>
2020-10-27 20:07:16 +1000

    finalised new user registration

--- src/components/NewUser/NewUserForm.jsx
+++ src/components/NewUser/NewUserForm.jsx
@@ -1,6 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 function NewUserForm() {
+    const [newUser, setNewUser] = useState({
+        username: "",
+        email: "",
+        password: "",
+        first_name: "",
+        last_name: "",
+        location: "",
+        about_me: "",
+    });
+
+    const history = useHistory();
+
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        setNewUser((prevUser) => ({
+            ...prevUser,
+            [id]: value,
+        }));
+    };
+
+    const postData = async () => {
+        const response = await fetch(
+            `${process.env.REACT_APP_API_URL}/users/`, 
+            {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newUser),
+        });
+        return response.json();
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        postData().then((response) => {
+            console.log(response);
+        history.push("/login");
+        });
+    };
+
     return (
         <form>
             <div>
@@ -9,6 +51,7 @@ function NewUserForm() {
                 type="text"
                 id="username"
                 placeholder="Enter a username"
+                onChange={handleChange}
                 />
             </div>
             <div>
@@ -17,6 +60,7 @@ function NewUserForm() {
                 type="text"
                 id="email"
                 placeholder="Enter your email address"
+                onChange={handleChange}
                 />
             </div>
             <div>
@@ -25,6 +69,7 @@ function NewUserForm() {
                 type="text"
                 id="password"
                 placeholder="Enter a password"
+                onChange={handleChange}
                 />
             </div>
             <div>
@@ -33,6 +78,7 @@ function NewUserForm() {
                 type="text"
                 id="first_name"
                 placeholder="First name"
+                onChange={handleChange}
                 />
             </div>
             <div>
@@ -41,6 +87,7 @@ function NewUserForm() {
                 type="text"
                 id="last_name"
                 placeholder="Last name"
+                onChange={handleChange}
                 />
             </div>
             <div>
@@ -49,6 +96,7 @@ function NewUserForm() {
                 type="text"
                 id="location"
                 placeholder="Location"
+                onChange={handleChange}
                 />
             </div>
             <div>
@@ -57,9 +105,10 @@ function NewUserForm() {
                 type="text"
                 id="about_me"
                 placeholder="Enter details about yourself"
+                onChange={handleChange}
                 />
             </div>
-            <button type="submit">
+            <button type="submit" onClick={handleSubmit}>
                 Sign me up!
             </button>
         </form>
