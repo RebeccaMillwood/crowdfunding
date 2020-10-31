8fd4826b580b813489cf15c0f1a98652b183d5ae
RebeccaMillwood <bec-millwood@live.com.au>
2020-10-31 12:22:20 +1000

    updated project page view to include project details

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "./ProjectPage.css";
 
 function ProjectPage() {
     const [projectData, setProjectData] = useState({ pledges: [] });
@@ -19,7 +20,11 @@ function ProjectPage() {
     return (
         <div>
             <h2>{projectData.title}</h2>
-            <h3>Created at {projectData.date_created}</h3>
+            <img src={projectData.image} alt=""/>
+            <h3>Project opened: {projectData.date_created}</h3>
+            <h3>Description: {projectData.description}</h3>
+            <h3>Location: {projectData.location}</h3>
+            <h3>Walks needed this month (in hours): {projectData.goal}</h3>
             <h3>{`Status: ${projectData.is_open}`}</h3>
             <h3>Pledges:</h3>
             <ul>
